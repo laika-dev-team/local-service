14708318d14a2d04fef2b0026271f9ed7e4de49e
shadowWalker <hieu811@gmail.com>
2023-07-05 23:19:26 +0700

    update env

--- src/config.ts
+++ src/config.ts
@@ -13,8 +13,8 @@ export const LOG_LEVEL = process.env.LOG_LEVEL || 'info'
 export const DEBUG = process.env.DEBUG === 'true'
 export const PORT = parseInt(process.env.PORT || '5556')
 export const SWAGGER_ENDPOINT = process.env.SWAGGER_ENDPOINT
-export const PRINTERS = (process.env.PRINTERS || 'tcp://192.168.1.234').split(
-  ';'
-)
+export const PRINTERS = (
+  process.env.PRINTERS || 'tcp://192.168.1.234:9100'
+).split(';')
 export const JOB_INTERVAL_MS = parseInt(process.env.JOB_INTERVAL_MS || '250')
 export const STORE_ID = parseInt(process.env.STORE_ID || '0')
