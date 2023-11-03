a13a76bc97301c56fdc3ec02a3c51e8b564f41f8
shadowWalker <hieu811@gmail.com>
2023-11-03 16:42:35 +0700

    increase table size

--- src/controller/printer.ts
+++ src/controller/printer.ts
@@ -206,10 +206,10 @@ export class PrinterController {
       'DIRECTION 1',
       'CLS',
       `TEXT ${x},60,"2",0,1,1,"${removeVietnameseTones(storeName)}"`,
-      `TEXT ${x},90,"1",0,1,1,"The       ${table} ${removeVietnameseTones(
+      `TEXT ${x},90,"2",0,1,1,"The       ${table} ${removeVietnameseTones(
         zone
       )}"`,
-      `TEXT ${x},110,"1",0,1,1,"Ngay gio  ${time.format('DD/MM/YY HH:mm')}"`,
+      `TEXT ${x},120,"1",0,1,1,"Ngay gio  ${time.format('DD/MM/YY HH:mm')}"`,
       `TEXT ${x},140,"3",0,1,1,"${removeVietnameseTones(name)}"`,
     ]
     let y = 140
